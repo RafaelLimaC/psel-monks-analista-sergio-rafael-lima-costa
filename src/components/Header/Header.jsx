2314187d5e150799f1a92960cc5b28089b1aa0c4
Rafael Lima <srafalimacosta@gmail.com>
2025-03-26 13:05:12 -0300

    feat: finishing mobile menu, animations and transitions

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./Header.scss";
 import Logo from "../../assets/logo.svg";
+import BackSVG from "../shared/BackSVG";
 
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -11,7 +12,7 @@ function Header() {
 
   return (
     <header>
-      <div className="header-wrapper">
+      <div className={`header-wrapper ${isMenuOpen ? "menu-open" : ""}`}>
         <img src={Logo} alt="Monks Logo" className="header-logo" />
         <nav className="header-nav">
           <ul className="header-nav-list">
@@ -32,7 +33,17 @@ function Header() {
         <label className="hamburger-menu" htmlFor="menu">
           <input type="checkbox" id="menu" onChange={handleMenuToggle} />
         </label>
-        <div className={`mobile-menu ${isMenuOpen ? "open" : ""}`}></div>
+      </div>
+      <div className={`mobile-menu ${isMenuOpen ? "open" : ""}`}>
+        <div className="mobile-menu-wrapper">
+          <ul>
+            <li>Categoria 1</li>
+            <li>Categoria 2</li>
+            <li>Categoria 3</li>
+            <li>Categoria 4</li>
+          </ul>
+          <BackSVG />
+        </div>
       </div>
     </header>
   );
